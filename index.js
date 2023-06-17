7487f459fb32c8ffb0a48150bcc40643f37578fa
hatsumi3 <al.usamiku39@gmail.com>
2023-06-17 18:04:23 +0900

    feat: add simple post method.

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
+app.post("/autumn", function (req, res) {
+  console.log("POSTリクエストの確認.");
+});
+
 app.listen(5000, function () {
   console.log("listening on localhost port 5000.");
 });
