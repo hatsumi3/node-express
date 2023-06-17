5730c1bdea29b227b5ef91930ecb4d4569b63ddd
hatsumi3 <al.usamiku39@gmail.com>
2023-06-17 17:55:38 +0900

    feat: add get function.

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+const coronaData = require("./coronaData.json");
 
-app.listen(5000, function(){
-    console.log("listening on localhost port 5000.")
-})
+app.get("/", function (req, res) {
+  res.send(coronaData);
+});
+
+app.listen(5000, function () {
+  console.log("listening on localhost port 5000.");
+});
